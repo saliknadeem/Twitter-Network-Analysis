14556d7f72263a593d58857905d25b3dd65dc0f7
Salik Nadeem <saliknadeem@gmail.com>
2019-04-11 17:35:52 -0400

    Plotly Tweet Location graph
    - Implemented a basic version of the plotly
    - Friends follow network being created accurately

---
+++ dataAnalysis.py
@@ -0,0 +1,160 @@
+
+import config ##custom config file to load API Keys
+
+import csv
+import pandas as pd
+import numpy as np
+
+
+from geopy.geocoders import Nominatim
+# from geopy.exc import GeocoderTimedOut
+from geopy.exc import GeopyError
+
+
+
+# import plotly.plotly as py
+# import plotly.graph_objs as go
+# from datetime import datetime
+#
+
+import plotly
+plotly.tools.set_credentials_file(username=config.plotly_username, api_key=config.plotly_api_key)
+
+# searchTweet = "#ONBudget"
+searchTweet = "#pakistan_Zindabad"
+
+
+df = pd.read_csv('data/'+searchTweet+'_tweets.csv')
+
+
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    print(df['Location'])
+
+
+# data = [go.Scatter(x=df.Date, y=df['Date'])]
+# py.plot(data, filename = 'time-series-simple')
+
+
+# geolocator = Nominatim()
+# for loc in df['Location']:
+#     try:
+#         #print(loc)
+#         location = geolocator.geocode(loc)
+#         #print(location)
+#         lat_long = {
+#             "type": "Point",
+#             "coordinates": [location.longitude, location.latitude]
+#         }
+#         print("loc=",loc," -- ",location.longitude,location.latitude)
+#     except (GeopyError, AttributeError):
+#         pass
+
+
+
+#Function used to geocode locations and override timeout error
+def do_geocode(address):
+    geopy = Nominatim(timeout=3)
+    try:
+        return geopy.geocode(address,exactly_one=True)
+    except GeocoderTimedOut:
+        return do_geocode(address)
+
+#Creating Geocoded Location column
+df['GeocodedLocation']=df['Location'].apply(lambda x: do_geocode(x) if x != None else None)
+
+
+#Create the Latitude Column
+lat=[]
+for i in df['GeocodedLocation']:
+    if i== None:
+        lat.append(None)
+    else:
+        lat.append(i.latitude)
+df['Latitude']=lat
+df['Latitude'].astype('float')
+
+#Create the Longitude Column
+long=[]
+for i in df['GeocodedLocation']:
+    if i== None:
+        long.append(None)
+    else:
+        long.append(i.longitude)
+df['Longitude']=long
+df['Longitude'].astype('float')
+
+
+#Drop GeocodedLocation Column
+df=df.drop(['GeocodedLocation'],axis=1)
+
+print(df)
+
+
+
+import plotly.graph_objs as go
+import plotly.plotly as py
+
+df['text'] = df['Username'] + '' + df['Location']
+
+scl = [ [0,"rgb(5, 10, 172)"],[0.35,"rgb(40, 60, 190)"],[0.5,"rgb(70, 100, 245)"],\
+    [0.6,"rgb(90, 120, 245)"],[0.7,"rgb(106, 137, 247)"],[1,"rgb(220, 220, 220)"] ]
+
+data = [ dict(
+        type = 'scattergeo',
+        locationmode = 'country names',
+        lon = df['Longitude'],
+        lat = df['Latitude'],
+        text = df['text'],
+        mode = 'markers',
+        marker = dict(
+            size = 8,
+            opacity = 0.8,
+            reversescale = True,
+            autocolorscale = False,
+            symbol = 'circle',
+            line = dict(
+                width=1,
+                color='rgba(102, 102, 102)'
+            ),
+            colorscale = scl,
+            cmin = 0,
+            color = 0.5,
+            # color = df_sum_arrivals['cnt'],
+            cmax = 1,
+            # cmax = df_sum_arrivals['cnt'].max(),
+            # colorbar=dict(
+            #     title="Number of Tweets"
+            # )
+        ))]
+
+layout = dict(
+        title = 'Tweets Based on available locaiton data',
+        colorbar = False,
+        # colorbar = True,
+        geo = dict(
+            # scope='usa',
+            projection= go.layout.geo.Projection(
+            type = 'equirectangular'),
+            showland = True,
+            landcolor = "rgb(250, 250, 250)",
+            subunitcolor = "rgb(217, 217, 217)",
+            countrycolor = "rgb(217, 217, 217)",
+            countrywidth = 0.5,
+            subunitwidth = 0.5
+        ),
+    )
+
+fig = dict( data=data, layout=layout )
+url = py.plot( fig, validate=False, filename='tweets' )
+
+
+
+
+
+
+# #Export Data to a csv
+# df.to_csv('data.csv', index=False)
+
+
+# users = df['Username'].tolist()
+# print(users)
--- getTweetData.py
+++ getTweetData.py
@@ -9,7 +9,6 @@ import numpy as np
 from collections import Counter
 from collections import defaultdict
 
-
 ##input your credentials here
 consumer_key = config.consumer_key
 consumer_secret = config.consumer_secret
@@ -20,10 +19,15 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 
+searchTweet = "#ONBudget"
+getUserData = 0
+
+
+
 api = tweepy.API(auth,wait_on_rate_limit=True) ##Free API issues, need to wait on rate limit
 
 # Open/Create a file to write data
-csvFile = open('data/tweets.csv', 'w', newline='')
+csvFile = open('data/'+searchTweet+'_tweets.csv', 'w', newline='',encoding='utf8')
 #Use csv Writer
 csvWriter = csv.writer(csvFile)
 
@@ -31,6 +35,7 @@ csvWriter = csv.writer(csvFile)
 tagList = [] #List of Hashtags
 currTagList = []
 urlList = [] #List of URLs in tweets
+currurlList = [] #List of URLs in tweets
 userList = [] #List of user IDs with matching tweets
 userSNList = [] #List of user Screen_names with matching tweets
 tweets = 0 # Tweet Counter
@@ -47,65 +52,69 @@ def paginate(iterable, page_size):
         yield page
 
 
-csvWriter.writerow(["user ID", "Username", "Location", "Date", "Tweet","hashtags","followers","friends"])
+csvWriter.writerow(["user ID", "Username", "Location", "Date", "Tweet","ReTweeted","hashtags","URLs","followers","friends"])
 
-
-for tweet in tweepy.Cursor(api.search,q="#pakistan_Zindabad", lang="en",tweet_mode='extended').items():
+rtCheck = 0
+for tweet in tweepy.Cursor(api.search,q=searchTweet, lang="en",tweet_mode='extended').items():
     for attrH in tweet.entities["hashtags"]:
     ###    print(attrH['text'])
         tagList.append(attrH['text'].encode("utf-8"))
         currTagList.append(attrH['text'].encode("utf-8"))
-    ###for attrU in tweet.entities["urls"]: #URL fetching code
-    ###    print(attrU["expanded_url"])
-    ###    urlList.append(attrU["expanded_url"])
-    #print("ID: ",tweet.user.id," - Screen_name: ",tweet.user.screen_name, " - location: ",tweet.user.location.encode('utf-8') )
+    for attrU in tweet.entities["urls"]: #URL fetching code
+       urlList.append(attrU["expanded_url"])
+       currurlList.append(attrU["expanded_url"])
     userList.append(tweet.user.id)
     userSNList.append(tweet.user.screen_name)
     userSN_ID[tweet.user.screen_name] = tweet.user.id
-    ###print (tweet.entities["hashtags"][0]['text'])
-    ###print('\n')
-    #print('\n')
-    #print (tweet.full_text)
-    #print('\n')
-    ###print (tweet.entities['urls'])
-    ###print('\n\n\n')
-    # print("---------------------------------------------------\n",tweet,"\n-----------------------------------------------------------------------\n")
+    if tweet.full_text.startswith('RT @'):
+        rtCheck = 1
+    else:
+        rtCheck = 0
+
+    # print("---------------------------------------------------\n",tweet.full_text,"\n-----------------------------------------------------------------------\n")
     tweets = tweets+1
     if tweets % 20 == 0:
-        print(tweets,'\n')
-    csvWriter.writerow([tweet.user.id, tweet.user.screen_name.encode("utf-8"), tweet.user.location.encode("utf-8"), tweet.created_at, tweet.full_text.encode("utf-8"),currTagList,tweet.user.followers_count,tweet.user.friends_count])
-
-
-    # print('user SN ID-----',userSN_ID)
-
-    # print("---------------------------------------------------\n",
-    #
-    #     "Tweet-",tweet.full_text.encode('utf-8'),"\n",
-    #     "\n Tweet.created_at-",tweet.created_at,"\n",
-    #     "Tweet.entities[\'hashtags\']-",currTagList,"\n",
-    #     "user.ID-",tweet.user.id,"\n",
-    #     "user.screen_name-",tweet.user.screen_name,"\n",
-    #     "user.location (use this)-",tweet.user.location,"\n",
-    #     "geo-",tweet.geo,"\n",
-    #     "coordinates-",tweet.coordinates,"\n",
-    #     "place-",tweet.place,"\n",
-    #     "user.followers_count-",tweet.user.followers_count,"\n",
-    #     "user.friends_count-",tweet.user.friends_count,"\n",
-    #
-    #     "\n-----------------------------------------------------------------------\n")
+        print("Tweets searched = ",tweets,'\n')
+    csvWriter.writerow([tweet.user.id, tweet.user.screen_name, tweet.user.location, tweet.created_at, tweet.full_text,rtCheck, currTagList,currurlList,tweet.user.followers_count,tweet.user.friends_count])
     currTagList = []
 
-#friends = tweepy.Cursor(api.followers_ids(userList))
-#print("followers -- ", friends)
+print("Total Tweets = ", tweets)
 
 csvFile.close()
 
 
 ###Code to store Network info
 # Open/Create a file to write data
-csvFile = open('data/users.csv', 'w', newline='')
-#Use csv Writer
-csvWriter = csv.writer(csvFile)
+
+
+
+
+
+if (getUserData):
+    csvFile = open('data/'+searchTweet+'_users.csv', 'w', newline='')
+    #Use csv Writer
+    csvWriter = csv.writer(csvFile)
+
+    iter=0
+    print("total unique people = ",len(userSN_ID.keys()))
+    from itertools import combinations
+    for key1, key2 in combinations(userSN_ID.keys(), r = 2):
+
+        # print(key1,key2)
+        link = api.show_friendship(source_screen_name=key1, target_screen_name=key2)
+        # print(link[0].following,link[0].followed_by)
+        if (link[0].followed_by):
+            print(key1,"->",key2)
+            csvWriter.writerow([key1.encode('utf-8'),key2.encode('utf-8')])
+        if (link[1].followed_by):
+            print(key2,"->",key1)
+            csvWriter.writerow([key2.encode('utf-8'),key1.encode('utf-8')])
+        iter = iter+1
+        if iter % 20 == 0:
+            print("Friends searched = ",iter)
+
+    # print("---------------------------------------------------\n",followersList,"\n-----------------------------------------------------------------------\n")
+    csvFile.close()
 
 
 ###### Old bad code
@@ -142,47 +151,9 @@ csvWriter = csv.writer(csvFile)
 
 
 
-######## NEW CODE FOR FRIENDSHIP RELATIONSHIP
-# counter=0
-# followersList = []
-# for sn,id in userSN_ID.items():
-#     print('for loop user\n')
-#     for follower in tweepy.Cursor(api.followers, user_id=id).items():
-#         counter=counter+1
-#         followersList.append([id,follower.screen_name,follower.id])
-#         csvWriter.writerow([sn.encode('utf-8'),id,follower.screen_name.encode('utf-8'),follower.id])
-#         print([sn,id,follower.screen_name.encode('utf-8'),follower.id])
-#     print("counter---",counter)
-
-# from itertools import permutations
-# for key1, key2 in permutations(userSN_ID.keys(), r = 2):
-
-from itertools import combinations
-for key1, key2 in combinations(userSN_ID.keys(), r = 2):
-    # print(key1,key2)
-    link = api.show_friendship(source_screen_name=key1, target_screen_name=key2)
-    # print(link[0].following,link[0].followed_by)
-    if (link[0].followed_by):
-        print(key1,"->",key2)
-        csvWriter.writerow([key1.encode('utf-8'),key2.encode('utf-8')])
-    if (link[1].followed_by):
-        print(key2,"->",key1)
-        csvWriter.writerow([key2.encode('utf-8'),key1.encode('utf-8')])
-
-
-
-# print("---------------------------------------------------\n",followersList,"\n-----------------------------------------------------------------------\n")
-
-# print(friendList.items())
-
-csvFile.close()
-
-
-
 print('\n')
 print(Counter(tagList),'\n')
-#print(Counter(urlList),'\n')
-print('Tweets=',tweets,'\n')
+# print(Counter(urlList),'\n')
 
 
 
